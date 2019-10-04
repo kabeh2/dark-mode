ce321de7638b68f72e505900dd7cc33812f96af4
kabeh2 <mattcnk@hotmail.com>
2019-10-03 22:39:32 -0400

    Updated readme and added build

---
+++ build/precache-manifest.6a1bd2d9e7f2ead5e1cea672d4caf308.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3561ab2d648e2241f61996d7911ec3f0",
+    "url": "/dark-mode/index.html"
+  },
+  {
+    "revision": "db537079179617b533ef",
+    "url": "/dark-mode/static/css/main.4648a9be.chunk.css"
+  },
+  {
+    "revision": "fbc2df4bcef86e1304f8",
+    "url": "/dark-mode/static/js/2.b9d43e23.chunk.js"
+  },
+  {
+    "revision": "db537079179617b533ef",
+    "url": "/dark-mode/static/js/main.39268e0f.chunk.js"
+  },
+  {
+    "revision": "b137549adc5314d5bf5b",
+    "url": "/dark-mode/static/js/runtime~main.dbe6e273.js"
+  }
+]);
